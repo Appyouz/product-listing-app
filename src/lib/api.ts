27dd766040c69139eac06c821f39c37a97f64287
appyouz <dikpalkhatri7@gmail.com>
2025-11-27 09:53:09 +0545

    refactor: accept page number

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,11 +3,11 @@ import { Product } from "@/types/product";
 
 const API_URL = "https://api.freeapi.app/api/v1/public/randomproducts";
 
-export const getProducts = async (): Promise<any[]> => {
+export const getProducts = async (page: number = 1): Promise<Product[]> => {
   try {
     const response = await axios.get(API_URL, {
       params: {
-        page: 1,
+        page: page,
         limit: 10,
         inc: "id,title,images,thumbnail,price,category",
         query: "watches",
